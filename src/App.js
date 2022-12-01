42699518c7322a60bcc0b39d7ffdb65bba06cb91
Jennifer Quispe <jenny.q@me.com>
2022-12-01 16:26:52 -0500

    adding animals state, svgs

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import AnimalShow from './AnimalShow'
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
+
+function App() {
+  const [animals, setAnimals] = useState([])
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+
+  const renderedAnimals = animals.map((animal,i) => {
+    return <AnimalShow type={animal} key={i} />
+  })
+
+  return (
+    <section>
+      <button onClick={handleClick}>Add Animal</button>
+      {renderedAnimals}
+    </section>
+  )
+}
+
+export default App;
